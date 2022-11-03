eb7c93839566841f7394dfe1c124640028a1ab88
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2022-11-03 19:08:07 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const cons = require('consolidate')
+// const cons = require('consolidate')
 // const serveIndex = require('serve-index');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const expressApp = require("express/lib/router");
+// const expressApp = require("express/lib/router");
 
 // instantiate express middleware
 const app = express();
@@ -17,26 +17,26 @@ const cfg = {
 }
 // Use node_modules scripts
 // CSS dependencies
-app.use('/css', express.static(path.join(__dirname, 'node_modules/aos/aos.css')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/css/bootstrap.min.css')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/bootstrap-icons.css')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/boxicons/css/boxicons.min.css')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/glightbox/css/glightbox.min.css')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/swiper/swiper-bundle.min.css')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/aos/')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/css/')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/boxicons/css/')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/glightbox/css/')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/swiper/')))
 // app.use('/css', express.static(path.join(__dirname, '')))
 // Javascript dependencies
 app.use('/js', express.static(path.join(__dirname, 'node_modules/aos/aos.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/purecounter/purecounter_vanilla.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/js/bootstrap.bundle.min.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/glightbox/js/glightbox.min.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/isotope-layout/isotope.pkgd.min.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/swiper/swiper-bundle.min.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/typed.js/typed.min.js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/waypoints/noframework.waypoints.js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/purecounter/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/js/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/glightbox/js/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/isotope-layout/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/swiper/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/typed.js/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/waypoints/')))
 // app.use('/js', express.static(path.join(__dirname, '')))
 
-// assign the swig engine to .html files
-app.engine(`html`, cons.swig);
+// assign the swig engine to parse .html file extensions
+app.engine(`html`, require('ejs').renderFile);
 // set the view directory and .html as the default extension
 app.set(`views`, path.join(__dirname, `views`));
 app.set(`view engine`, `html`);
@@ -44,9 +44,9 @@ app.set(`view engine`, `html`);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(`/`, express.static(path.join(__dirname, 'public')));
-app.use(`/js`, indexRouter);
+// app.use(cookieParser());
+app.use(`/`,  express.static(path.join(__dirname, 'public')));
+app.use(`/`, indexRouter);
 app.use(`/users`, usersRouter);
 
 app.listen(
