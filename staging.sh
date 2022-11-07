756135770153c44c6843309555bb8b1c6ba2b5c9
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2022-11-07 05:22:04 -0500

    setup pages

--- staging.sh
+++ staging.sh
@@ -12,23 +12,24 @@ mkdir -p  staging/js \
           staging/img/testimonials
 
 # state all boxicons fonts
-cp -R assets/vendor/boxicons/fonts/** staging/fonts/ && \
-cp -R assets/vendor/bootstrap-icons/fonts/** staging/fonts/ && \
+cp -R assets/vendor/boxicons/fonts/ staging/fonts/ && \
+cp -R assets/vendor/bootstrap-icons/fonts/ staging/fonts/ && \
 
 # stage all js files
-cp -R assets/js/** staging/js/ && \
+cp -r assets/js/ staging/js/ && \
 cp assets/vendor/aos/aos.js staging/js/ && \
 cp assets/vendor/purecounter/dist/purecounter_vanilla.js staging/js/ && \
-cp -R assets/vendor/bootstrap/js/** staging/js/ && \
-cp -R assets/vendor/glightbox/js/** staging/js/ && \
+cp -r assets/vendor/bootstrap/js/** staging/js/ && \
+cp -r assets/vendor/glightbox/js/** staging/js/ && \
 cp assets/vendor/isotope-layout/isotope.pkgd.min.js staging/js/ && \
 cp assets/vendor/swiper/swiper-bundle.min.js staging/js/ && \
 cp assets/vendor/typed.js/typed.min.js staging/js/ && \
 cp assets/vendor/waypoints/noframework.waypoints.js staging/js/ && \
 cp assets/vendor/php-email-form/validate.js staging/js/ && \
+cp -r assets/vendor/webfont/ staging/js/ && \
 
 # stage all css files
-cp -R assets/css/** staging/css/ && \
+cp -r assets/css/** staging/css/ && \
 cp assets/vendor/aos/aos.css staging/css/ && \
 cp assets/vendor/bootstrap/css/bootstrap.min.css staging/css/ && \
 cp assets/vendor/bootstrap/css/bootstrap.min.css.map staging/css/ && \
@@ -36,14 +37,14 @@ cp assets/vendor/bootstrap-icons/bootstrap-icons.css staging/css/ && \
 cp assets/vendor/boxicons/css/boxicons.min.css staging/css/ && \
 cp assets/vendor/glightbox/css/glightbox.min.css staging/css/ && \
 cp assets/vendor/swiper/swiper-bundle.min.css staging/css/ && \
-cp assets/vendor/webfont/webfont.js staging/js/ && \
+
 
 # stage all scss files
-cp -R assets/scss/ staging/scss/ && \
+cp -r assets/scss/ staging/scss/ && \
 
 # stage all image files
 cp -R assets/img/** staging/img/ && \
-cp -R views/** staging/
+cp -r views/** staging/
 return 0
 }
 
@@ -54,5 +55,5 @@ __get_css_files(){
   done
 }
 
-__setup_staging
+__setup_staging &&
 echo "Staging github-pages files complete..."
