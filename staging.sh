9468687582526767615918aa665fd398aeaa72d9
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2022-11-08 01:50:54 -0500

    setup pages

--- staging.sh
+++ staging.sh
@@ -11,37 +11,37 @@ mkdir -p  staging/js \
           staging/img && \
 
 # state all boxicons fonts
-cp -r assets/vendor/boxicons/fonts/ staging/fonts && \
-cp -r assets/vendor/bootstrap-icons/fonts/ staging/fonts && \
+cp -r assets/vendor/boxicons/fonts/ staging/fonts/ && \
+cp -r assets/vendor/bootstrap-icons/fonts/ staging/fonts/ && \
 
 # stage all js files
 cp -r assets/js/ staging/js/ && \
-cp -r assets/vendor/aos/aos.js staging/js && \
-cp -r assets/vendor/purecounter/dist/*.js staging/js && \
-cp -r assets/vendor/bootstrap/js/*.js staging/js && \
-cp -r assets/vendor/glightbox/js/*.js staging/js && \
-cp -r assets/vendor/isotope-layout/*.js staging/js && \
-cp -r assets/vendor/swiper/*.js staging/js && \
-cp -r assets/vendor/typed.js/*.js staging/js && \
-cp -r assets/vendor/waypoints/*.js staging/js && \
-cp -r assets/vendor/php-email-form/*.js staging/js && \
-cp -r assets/vendor/webfont/*.js staging/js && \
+cp -r assets/vendor/aos/aos.js staging/js/ && \
+cp -r assets/vendor/purecounter/dist/*.js staging/js/ && \
+cp -r assets/vendor/bootstrap/js/*.js staging/js/ && \
+cp -r assets/vendor/glightbox/js/*.js staging/js/ && \
+cp -r assets/vendor/isotope-layout/*.js staging/js/ && \
+cp -r assets/vendor/swiper/*.js staging/js/ && \
+cp -r assets/vendor/typed.js/*.js staging/js/ && \
+cp -r assets/vendor/waypoints/*.js staging/js/ && \
+cp -r assets/vendor/php-email-form/*.js staging/js/ && \
+cp -r assets/vendor/webfont/*.js staging/js/ && \
 
 # stage all css files
-cp -r assets/css/*.css staging/css && \
-cp -r assets/vendor/aos/*.css staging/css && \
-cp -r assets/vendor/bootstrap/css/*.css staging/css && \
-cp -r assets/vendor/bootstrap/css/*.css.map staging/css && \
-cp -r assets/vendor/bootstrap-icons/*.css staging/css && \
-cp -r assets/vendor/boxicons/css/*.css staging/css && \
-cp -r assets/vendor/glightbox/css/*.css staging/css && \
-cp -r assets/vendor/swiper/*.css staging/css && \
+cp -r assets/css/*.css staging/css/ && \
+cp -r assets/vendor/aos/*.css staging/css/ && \
+cp -r assets/vendor/bootstrap/css/*.css staging/css/ && \
+cp -r assets/vendor/bootstrap/css/*.css.map staging/css/ && \
+cp -r assets/vendor/bootstrap-icons/*.css staging/css/ && \
+cp -r assets/vendor/boxicons/css/*.css staging/css/ && \
+cp -r assets/vendor/glightbox/css/*.css staging/css/ && \
+cp -r assets/vendor/swiper/*.css staging/css/ && \
 
 # stage all scss files
-cp -r assets/scss/*.scss staging/scss && \
+cp -r assets/scss/*.scss staging/scss/ && \
 
 # stage all forms
-cp -r assets/forms/ staging/forms && \
+cp -r assets/forms/ staging/forms/ && \
 
 # stage all image files
 cp -r assets/img/ staging/img && \
