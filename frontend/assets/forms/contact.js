4fd5a03a7a429dcec7766743a5920ae4cfb022f3
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2023-11-12 13:05:09 -0500

    updated gh-pages deployment

---
+++ frontend/assets/forms/contact.js
@@ -0,0 +1,18 @@
+
+// Try mailslurp: https://www.npmjs.com/package/mailslurp-client
+// Try emailjs: https://www.npmjs.com/package/emailjs
+
+// Mailslurp
+const smtp = require('smtp');
+function sendEmail() {
+    Email.send({
+        Host : "smtp.mailslurp.com",
+        Username : "<mailslurp username>",
+        Password : "<mailslurp password>",
+        To : 'recipient@example.com',
+        From : "sender@example.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(message => alert(message)
+    );
+}
